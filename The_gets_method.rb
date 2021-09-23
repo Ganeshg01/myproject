5746b4fbecd3d6e64786d8dc88867f797877bca8
Ganeshg01 <ganesh.gajakosh@amagi.com>
2021-09-23 18:36:34 +0530

    First Commit testing

---
+++ The_gets_method.rb
@@ -0,0 +1,7 @@
+puts "Hi, What is your name?"
+name = gets.chomp
+
+puts "Great! What is your age?"
+age = gets.chomp.to_i
+
+puts "So your name is #{name} and you are #{age} years old."
---
+++ arithmatic.rb
@@ -0,0 +1,6 @@
+p 1+4    
+p 4+5
+=begin
+    
+sjfalsjfaflksaf    
+=end

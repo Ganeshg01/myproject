737119c4697813df4d346c53bad1be0e3a2071a7
Ganeshg01 <ganesh.gajakosh@amagi.com>
2021-09-27 01:15:15 +0530

    methods

--- arithmatic.rb
+++
@@ -1,6 +0,0 @@
-p 1+4    
-p 4+5
-=begin
-    
-sjfalsjfaflksaf    
-=end
---
+++ intro_to_blocks.rb
@@ -0,0 +1,6 @@
+# use the times methode  to output  the first 
+# 10 multiple of  3 
+
+10.times do |count|
+    puts "multiple of 3 are  #{3 *(count+1)}"
+end

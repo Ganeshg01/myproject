fcb0bfe0a7a09ee0966c656bab32e63052f717dd
Ganeshg01 <ganesh.gajakosh@amagi.com>
2021-09-29 23:39:46 +0530

    upto_downto

---
+++ upto_downto_methods.rb
@@ -0,0 +1,15 @@
+5.downto(0) do |current_number|
+    puts "we are currently on #{current_number}"
+    puts "yesss"
+end
+puts ""
+puts "liftoff!"
+
+puts"--------------------------------------------------------------------"
+
+0.upto(5) do |current_level|
+    puts "we are currently on #{current_level}"
+    puts "Up"
+end
+puts""
+puts"top"
